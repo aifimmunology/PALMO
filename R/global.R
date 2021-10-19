9620eff013eef2b8b92397b6965642234386d3ad
suhasv <73250895+suhasaii@users.noreply.github.com>
2021-10-19 12:02:09 -0700

    Delete R directory

--- R/global.R
+++
@@ -1,5 +0,0 @@
-utils::globalVariables(c("CV", "Freq", "PTID", "Sample", "Time", "UMAP_1",
-                         "UMAP_2", "Var2", "createLogFile", "getArchRThreads",
-                         "getGroupSE", "group", "metaData", "now", "pb",
-                         "totolIter", "value", ".", "..density..", "cv_res",
-                         "tSNE1", "tSNE2","ggline", "rcorr"))
