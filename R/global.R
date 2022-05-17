e09fae6ba481c236f70de98e5afb4f6ce2911f36
suhasv <73250895+suhasaii@users.noreply.github.com>
2022-05-17 09:47:39 -0700

    Add files via upload

---
+++ R/global.R
@@ -0,0 +1,8 @@
+utils::globalVariables(c("CV", "cv", "Freq", "PTID", "Sample", "Time", "UMAP_1", "UMAP_2", "Var2", 
+    "createLogFile", "getArchRThreads", "getGroupSE", "group", "metaData", "now", "pb", "totolIter", 
+    "value", ".", "..density..", "Gene", "Pr(>Chisq)", "barcodes", "column_to_rownames", "component", 
+    "contrast", "cv_res", "gather", "ggline", "m", "nomP", "num_cells_expressed", "primerid", 
+    "rcorr", "rownames_to_column", "spread", "tSNE1", "tSNE2", "meanDev", "outlier", "ggboxplot", 
+    "groupColumn", "includeGroup", "clusterBy", "binom.test", "pnorm", "signP", "z", "direction", 
+    "label", "group_oi", "na.omit", "time", "blank", "gene", "filter.NA", "filter.std", "standardise", 
+    "standardise2", "mestimate", "mfuzz", "mfuzz.plot", "bygroup", "rainbow", "feature", "variable"))
